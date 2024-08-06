4bd6b77427a0f3be9e2d5579a30028e81b1a928b
xingmingkongbai <zht_3456@qq.com>
2024-08-06 23:39:55 +0800

    Add files via upload

---
+++ 张浩天_8.3.py
@@ -0,0 +1,41 @@
+my_list = ["黑马程序员","传智播客","itheima","itcast","best","黑马程序员","传智播客","itheima","itcast"]
+my_ste = set()
+for i in my_list:
+    my_ste.add(i)
+print(my_ste)
+
+
+info_dict = {
+    "王力宏":{
+        "部门":"科技部",
+        "工资":3000,
+        "级别":1
+    },
+    "周杰伦":{
+        "部门":"市场部",
+        "工资":5000,
+        "级别":2
+    },
+    "林俊杰":{
+        "部门": "市场部",
+        "工资": 7000,
+        "级别": 3
+    },
+    "张学友":{
+        "部门": "科技部",
+        "工资": 4000,
+        "级别": 1
+    },
+    "刘德华":{
+        "部门": "市场部",
+        "工资": 6000,
+        "级别": 2
+    }
+}
+for name in info_dict:
+    if info_dict[name]["级别"]==1:
+        e_info_dict = info_dict[name]
+        e_info_dict["级别"]+=1
+        e_info_dict["工资"]+=1000
+        info_dict[name]=e_info_dict
+print(info_dict)
