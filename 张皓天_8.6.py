16b49916333204e197252266a89690f82c1575d2
xingmingkongbai <zht_3456@qq.com>
2024-08-09 23:05:53 +0800

    Add files via upload

---
+++ 张皓天_8.6.py
@@ -0,0 +1,20 @@
+def test_func(compute):
+    result = compute(1,2)
+    print(result)
+test_func(lambda x,y:x+y)
+
+f = open("D:/test.py","r",encoding="utf-8")
+content = f.read()
+count = content.count("itheima")
+print(count)
+f.close()
+
+import my_package.module1
+import my_package.module2
+my_package.my_module1.info_print1()
+my_package.my_module2.info_print2()
+
+from my_package import module1
+from my_package import module2
+module1.info_print1()
+module2.info_print2()
