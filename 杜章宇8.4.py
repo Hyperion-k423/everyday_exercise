e623f0906e2e4341f0a157fd436148e3149667e8
Logic_006 <2499748395@qq.com>
2024-08-12 17:38:36 +0800

    Add files via upload

---
+++ 杜章宇8.4.py
@@ -0,0 +1,17 @@
+# 数据类型转换
+list()
+str()
+set()
+tuple()
+# 正向排序
+sorted()
+# 反向排序
+sorted(, reverse=True)
+# 字符串的大小比较
+# ASCII码值比较大小
+# 函数多返回值
+return(1, 2)
+# 用多个变量接收
+# 函数多种传参方式
+# 函数传递函数
+# 匿名函数，使用一次，用lambda
